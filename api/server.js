440827a0040e4441b9ec7a70fd41fa70a64bba65
Anubhav <dhuliya.a@gmail.com>
2022-01-16 01:12:50 +0530

    Added server.js

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const initRoutes = require("./routes.js");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+let port = 8080;
+
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
