377b6b0bc65ffbd3457511fefcd2863129347e83
ac491 <ac491@snu.edu.in>
2022-01-16 23:10:11 +0530

    update cors property

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config()
 const initRoutes = require("./routes.js");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors());
+
 initRoutes(app);
 
 app.listen(process.env.PORT, () => {
