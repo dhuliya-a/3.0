980ad3c54ef3bbc8791e4c872d922321bd8490fd
Anubhav <dhuliya.a@gmail.com>
2022-01-16 18:01:55 +0530

    User login and collecion details screens added

---
+++ layer-mint/src/components/user-label/userLabel.js
@@ -0,0 +1,12 @@
+import './userLabel.css';
+
+function UserLabel() {
+    return (
+      <div className="user-label">
+        <h1 className="user-title">Hi, adhuliya</h1>
+      </div>
+    );
+  }
+  
+  export default UserLabel;
+  
